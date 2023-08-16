fc0224361ac60731167c31a53cbc2e6c950def54
chapmanayden <80990481+chapmanayden@users.noreply.github.com>
2023-08-16 11:45:10 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,33 @@
-function nextStep(stepId) {
-    // Hide all steps
-    const steps = document.querySelectorAll('.step');
-    for (let step of steps) {
-        step.style.display = 'none';
+var stepHistory = []; // Array to hold steps user has visited
+
+function proceed(stepId) {
+    // Get the current active step
+    var activeStep = document.querySelector('.step.active');
+
+    // If there's an active step, add its ID to the history
+    if (activeStep) {
+        stepHistory.push(activeStep.id);
     }
 
-    // Show the specified step
-    document.getElementById(stepId).style.display = 'block';
+    // Hide all steps
+    var steps = document.querySelectorAll('.step');
+    for (var i = 0; i < steps.length; i++) {
+        steps[i].classList.remove('active');
+    }
+    // Show the desired step
+    document.getElementById(stepId).classList.add('active');
+}
+
+function goBack() {
+    if (stepHistory.length > 0) {
+        // Hide all steps
+        var steps = document.querySelectorAll('.step');
+        for (var i = 0; i < steps.length; i++) {
+            steps[i].classList.remove('active');
+        }
+        
+        // Get the last step from the history and display it
+        var lastStepId = stepHistory.pop();
+        document.getElementById(lastStepId).classList.add('active');
+    }
 }
